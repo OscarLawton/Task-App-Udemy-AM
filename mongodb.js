aa39f5fea77583c64ec69236bbaa6c8fbe036b6c
Emmet <emmetlawton@gmail.com>
2019-12-01 23:34:58 +0000

    second commit

--- mongodb.js
+++ mongodb.js
@@ -12,4 +12,30 @@ MongoClient.connect(connectionURL, {userNewUrlParser: true}, (err, client) => {
     }
 
     console.log('Connected correctly!');
+
+    const db = client.db(databaseName);
+
+    /* db.collection('users').insertOne({
+        name: "Andrew",
+        age: 27
+    }), (err, result) => {
+        if(err){
+            return console.log('Unable to insret')
+        }
+    } */
+
+   /*  db.collection('users').insertMany([{name: "john doe", age: 31, }, {name:"Jason", age: 66}], (err, result) => {
+        if(err){
+            return console.log("unage to insert data!")
+        }
+        console.log("yeah it was added;")
+    }) */
+
+    db.collection('tasks').insertMany([{des: "Clean the house", completed: true}, {des: "take out the trash", completed: false }, {des: "do shopping", completed: false}], (err, result)=> {
+        if(err){
+            return console.log("could not insert data")
+        }
+        
+        console.log("data added to database")
+    })
 });

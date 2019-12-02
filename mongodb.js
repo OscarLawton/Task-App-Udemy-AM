6c9116e0f5413de009439106edc9b8af979093ad
Emmet <emmetlawton@gmail.com>
2019-12-02 17:36:05 +0000

    find One function for mongo

--- mongodb.js
+++ mongodb.js
@@ -9,36 +9,17 @@ const id = new ObjectID()
 console.log(id)
 console.log(id.getTimestamp())
 MongoClient.connect(connectionURL, {userNewUrlParser: true}, (err, client) => {
-    if(err){
-        return console.log('Unable to conncect to database!');
-    }
-
-    console.log('Connected correctly!');
+   if(err){
+       return console.log('Unable to connect to database!')
+   } 
 
     const db = client.db(databaseName);
 
-     db.collection('users').insertOne({
-        _id: id,
-        name: "Bobby",
-        age: 16
-    }), (err, result) => {
+    db.collection('users').findOne({ name: 'Bobby'}, (err, user) => {
         if(err){
-            return console.log('Unable to insret')
+            return console.log("unable get user")
         }
-    } 
 
-   /*  db.collection('users').insertMany([{name: "john doe", age: 31, }, {name:"Jason", age: 66}], (err, result) => {
-        if(err){
-            return console.log("unage to insert data!")
-        }
-        console.log("yeah it was added;")
-    }) */
-
-    /* db.collection('tasks').insertMany([{des: "Clean the house", completed: true}, {des: "take out the trash", completed: false }, {des: "do shopping", completed: false}], (err, result)=> {
-        if(err){
-            return console.log("could not insert data")
-        }
-        
-        console.log("data added to database")
-    }) */
+        console.log(user);
+    })
 });

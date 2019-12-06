9b269ef6c5c4b298eb93fc08ccfe6f40bbe1af9f
Emmet <emmetlawton@gmail.com>
2019-12-06 18:32:51 +0000

    sterilization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 mongoose.connect('mongodb://127.0.0.1:27017/task-app-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-const Task = mongoose.model("Task", {
+/* const Task = mongoose.model("Task", {
     name: { 
         type: String
     },
@@ -22,19 +23,43 @@ washingUp.save().then(() => {
     console.log("saved ", washingUp )
 }).catch((err) => {
     console.log(err)
-})
-/* const User = mongoose.model('User', {
+}) */
+ const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("email is invalid");
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a postive number');
+            }
+        }
+    },
+    phoneNumber: {
+        type: String,
+        default: "Nan"
     }
 });
 
 const me = new User({
-    name: "Emmet",
-    age: 31
+    name: "    Andy",
+    email: "andy@google.com",
+    
+    
 });
 
 me.save().then(() => {
@@ -42,4 +67,4 @@ me.save().then(() => {
 }).catch((err) => {
   console.log("error!", err)  
 })
- */
+ 

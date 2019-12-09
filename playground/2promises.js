c010a8d0e851b2e4becd969794e2642a8977cb3d
Emmet <emmetlawton@gmail.com>
2019-12-09 02:10:05 +0000

    async delete and count challenge

--- playground/2promises.js
+++ playground/2promises.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete("5deb10edc9745d20195ddc5f").then((task) => {
+Task.findByIdAndUpdate("5dea85594e4d4006d15afc7d", {completed: false}).then((task) => {
     console.log(task)
     return Task.countDocuments({completed: false})
 }).then((result) =>{
     console.log(result)
 }).catch((e)=>{
     console.log(e)
-})
+}) 
+/* 
+const deleteTaskAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed})
+    return count
+}
+
+deleteTaskAndCount("5dead57677e8d313854c716d", false).then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(e)
+}) */

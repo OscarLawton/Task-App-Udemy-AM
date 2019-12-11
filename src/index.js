3f569b2f800df8ea5b5a49a39186b77c0ce20602
Emmet <emmetlawton@gmail.com>
2019-12-11 14:49:11 +0000

    added bcrypt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000
 
 
-app.use(express.json())
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
@@ -19,3 +17,17 @@ app.get("/", (req, res)=>{
 app.listen(port, () => {
     console.log("Server is up on port " + port)
 })
+
+const bcrypt = require('bcrypt')
+const myFunction = async () => {
+    const password = 'Read12345!'
+    const hashedPassword = await bcrypt.hash(password, 8);
+    
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    console.log(isMatch)
+}
+
+myFunction()

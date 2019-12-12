94e394ace104121d2753c347a4737e85da17b624
Emmet <emmetlawton@gmail.com>
2019-12-12 13:45:34 +0000

    express middleware

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    res.status(503).send("site maintence")
+})
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
 
 
 app.get("/", (req, res)=>{

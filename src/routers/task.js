efc11936119c0360d6157524541b1cc16f0f3450
Emmet <emmetlawton@gmail.com>
2019-12-26 16:01:32 +0000

    sort by ascening or descending

--- src/routers/task.js
+++ src/routers/task.js
@@ -8,11 +8,17 @@ app.use(router)
 
 // GET /tasks?complete=false
 // GET /tasks?limit=10&skip=0
+// Get /tasks/?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-
+    const sort = {}
     if(req.query.completed){
         match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ?  -1 : 1
     }
     try{
         // New way (populate)
@@ -21,7 +27,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
